504942a97a43789f9ad737cb6502592c7f4f15e4
Cristina Hyun Ah <cristinahyunah@gmail.com>
2021-06-11 17:09:48 -0300

    Script carousel Teams

--- script.js
+++ script.js
@@ -16,7 +16,24 @@ $(document).ready(function(){
     
     //owl carousel sript
     $('.carousel').owlCarousel({
-
+        margin: 20,
+        loop: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsive:{
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
 
     });
 });
