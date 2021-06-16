95da39ee639d2e7f067fe2bb3c09d16a32071984
Cristina Shin <cristinahyunah@gmail.com>
2021-06-16 14:53:59 -0300

    mesma funcão do typing animacao profissão p/ sobre

--- script.js
+++ script.js
@@ -34,6 +34,12 @@ $(document).ready(function(){
         loop: true
     });
 
+    var typed = new Typed(".typing-2",{
+        strings: ["Ilustradora", "Designer", "Developer", "Lettering Artist"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
 
     //owl carousel sript
     $('.carousel').owlCarousel({
